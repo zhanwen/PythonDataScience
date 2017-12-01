8151b3590a66f478dd785277651833d23e68f3c2
hanwen <1106002609@qq.com>
2017-12-01 21:09:57 +0800

    add array sort

---
+++ chapter5/Unit27/ArraySort.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+sap = np.array(["MMM", "ABT", "ABBV", "ACN", "ACE", "ATVI", "ADBE", "ADT"])
+stocks = np.array(list([140.49, 0.97, 40.68, 41.53, 55.7, 57.21, 98.2, 99.19,
+                        109.96, 111.47, 35.71, 36.27, 87.85, 89.11, 30.22, 30.91]))
+
+stocks = stocks.reshape(8, 2).T
+
+result = sap[np.abs(stocks[0]-stocks[1]) > np.mean(np.abs(stocks[0]-stocks[1]))]
+print(result)
