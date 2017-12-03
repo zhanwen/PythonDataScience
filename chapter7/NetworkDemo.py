f8671945d866224f995158d97ea184d5a93f2f74
hanwen <1106002609@qq.com>
2017-12-03 20:19:01 +0800

    add network file operator

--- chapter7/NetworkDemo.py
+++ chapter7/NetworkDemo.py
@@ -1,13 +1,11 @@
 import networkx as nx
+import pandas as pd
 
+
+#使用 networkx
 borders = nx.Graph()
-not_borders1 = nx.DiGraph()
+# # not_borders1 = nx.DiGraph()
 # not_borders2 = nx.MultiDiGraph()
-
-# clustering() 函数不能作用在有向图上
-# nx.clustering(not_borders1) //wrong
-nx.clustering(nx.Graph(not_borders1))
-
 
 borders.add_node("Zimbabwe")
 borders.add_nodes_from(["Lugandon", "Zambia", "Portugal", "Kuwait", "Colombia"])
@@ -17,10 +15,15 @@ borders.add_edge("Zambia", "Zimbabwe")
 borders.add_edges_from([("Uganda", "Rwanda"), ("Uganda", "Kenya"), ("Uganda", "South Sudan"),
                         ("Uganda", "Tanzania"), ("Uganda", "Democratic Republic of the Congo")])
 
-# write
-with open("borders.graphml", "wb") as netfile:
-    nx.write_pajek(borders, netfile)
+# print(len(borders))
 
-# read
-with open("borders.graphml", "rb") as netfile:
-    borders = nx.read_pajek(netfile)
+print(borders.nodes)
+print(borders.node)
+print(borders.edges)
+print(borders.neighbors("Uganda"))
+print(borders.degree("Uganda"))
+print(borders.degree())
+
+degrees = pd.DataFrame(list(borders.degree().items()), columns=("country", "degree")).set_index("country")
+print(degrees.sort("degree").tail(5))
+
--- chapter7/NetworkxDemo.py
+++
@@ -1,3 +0,0 @@
-import networkx as nx
-
-#使用networkx
