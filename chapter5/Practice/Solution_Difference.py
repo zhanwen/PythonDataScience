c808eea061f14a6fd6e90da0cc1753772acc2c3b
hanwen <1106002609@qq.com>
2017-12-04 22:59:47 +0800

    solution difference

--- chapter5/Practice/Solution_Difference.py
+++ chapter5/Practice/Solution_Difference.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-#
+# # Some synthetic data for testing
 array = np.random.binomial(5, 0.5, size=100)
 print(array[1:])
 print(array[:-1])
+
+# The partial differences: slicing & broadcasting!
 diff = array[1:] - array[:-1]
 
 print(diff)
