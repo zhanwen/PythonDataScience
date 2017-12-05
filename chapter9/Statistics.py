a228eaa7e32a4566d0ea611242bcbede61617d8f
hanwen <1106002609@qq.com>
2017-12-05 23:08:30 +0800

    add statistics

--- chapter9/Statistics.py
+++ chapter9/Statistics.py
@@ -1,3 +1,16 @@
 import numpy.random as rnd
 
 #使用numpy来创建数据
+
+r1 = rnd.uniform(low=0.0, high=1.0, size=None)
+print(r1)
+
+# rnd.rand(shape)  相当于uniform(0.0, 1.0, None)
+# r2 = rnd.randint(low=None, high=None, size=None)
+r3 = rnd.normal(loc=0.0, scale=1.0, size=None)
+print(r3)
+
+# rnd.randn(shape) 等同于normal(0.0, 1.0, shape)
+# rnd.binomial(n, p, size=None)
+
+
