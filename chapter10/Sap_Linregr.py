22ccacbc1881e233fa9ba7d313881963ff47c3bd
hanwen <1106002609@qq.com>
2017-12-06 20:06:48 +0800

    add logit example

---
+++ chapter10/Sap_Linregr.py
@@ -0,0 +1,45 @@
+# START:setup
+import numpy, pandas as pd
+import matplotlib, matplotlib.pyplot as plt
+import sklearn.linear_model as lm
+
+# Get the data
+sap = pd.read_csv("sapXXI.csv").set_index("Date")
+# END:setup
+
+# START:select
+# Select a "linearly looking" part
+sap.index = pd.to_datetime(sap.index)
+sap_linear = sap.ix[sap.index > pd.to_datetime('2009-01-01')]
+
+# Prepare the model and fit it
+olm = lm.LinearRegression()
+X = numpy.array([x.toordinal() for x in sap_linear.index])[:, numpy.newaxis]
+y = sap_linear['Close']
+olm.fit(X, y)
+
+# Predict values
+yp = [olm.predict(x.toordinal())[0] for x in sap_linear.index]
+
+# Evaluate the model
+olm_score = olm.score(X, y)
+# END:select
+
+# START:plot
+# Select a nice plotting style
+matplotlib.style.use("ggplot")
+
+# Plot both data sets
+plt.plot(sap_linear.index, y)
+plt.plot(sap_linear.index, yp)
+
+# Add decorations
+plt.title("OLS Regression")
+plt.xlabel("Year")
+plt.ylabel("S&P 500 (closing)")
+plt.legend(["Actual", "Predicted"], loc="lower right")
+plt.annotate("Score=%.3f" % olm_score, 
+             xy=(pd.to_datetime('2010-06-01'), 1900))
+
+plt.savefig("../images/sap-linregr.pdf")
+# END:plot
