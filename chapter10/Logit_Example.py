33ff0591f8567dfb83c9e9c119321dc2d5ff377a
hanwen <1106002609@qq.com>
2017-12-07 23:00:22 +0800

    update

--- chapter10/Logit_Example.py
+++ chapter10/Logit_Example.py
@@ -8,14 +8,14 @@ clf = lm.LogisticRegression(C=10.0)
 # Read the data sheet, quantize letter grades
 grades = pd.read_table("grades.csv")
 labels = ('F', 'D', 'C', 'B', 'A')
-grades["Letter"] = pd.cut(grades["Final score"], [0, 60, 70, 80, 90, 100], labels=labels)
+grades["Letter"] = pd.cut(grades["Final score"], [0, 40, 50, 60, 70, 100], labels=labels)
 
-X = grades[["Quiz 1", "Quiz 2"]]
+X = grades[["Quiz 1", "Quiz 2", "Quiz 3"]]
 
 # Fit the model, display the score and the confusion matrix
 clf.fit(X, grades["Letter"])
 
 print("Score=%.3f" % clf.score(X, grades["Letter"]))
-cm = confusion_matrix(clf.predict(X), grades["Letter"])
 
+cm = confusion_matrix(clf.predict(X), grades["Letter"])
 print(pd.DataFrame(cm, columns=labels, index=labels))
